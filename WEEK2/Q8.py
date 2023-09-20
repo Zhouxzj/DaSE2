125e4cc8606dc8acfc78a6ac2cf01a06e762fbda
Zhouxzj <Zhouxzj@163.com>
2023-09-21 00:21:18 +0800

    new commit for week 2(Dase homework)

---
+++ WEEK2/Q8.py
@@ -0,0 +1,14 @@
+def find_value_pai_1(num):
+    s = 0
+    for i in range(num):
+        s += (-1) ** i * (1 / (2 * i + 1))
+    s = 4 * s
+    print("第一种方法的结果为%.8f" % s)
+    return
+
+
+def find_value_pai_2():
+    return
+
+
+find_value_pai_1(10000000)
